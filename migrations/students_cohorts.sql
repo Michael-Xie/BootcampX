9873a8b7b8c611272b2c7d6126b3645c8a014176
Michael-Xie <xiemicha@gmail.com>
2020-01-06 14:38:40 +0000

    Updated data type definitiions for students and cohorts table

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,17 +1,35 @@
+-- CREATE TABLE cohorts (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(100),
+--   start_date DATE,
+--   end_date DATE
+-- );
+
+-- CREATE TABLE students (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(100),
+--   email VARCHAR(150),
+--   phone VARCHAR(20),
+--   github VARCHAR(200),
+--   start_date DATE,
+--   end_date DATE,
+--   cohort_id INTEGER
+-- );
+
 CREATE TABLE cohorts (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE students (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  email VARCHAR(150),
-  phone VARCHAR(20),
-  github VARCHAR(200),
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
   start_date DATE,
   end_date DATE,
-  cohort_id INTEGER
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
